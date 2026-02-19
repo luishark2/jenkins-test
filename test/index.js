eb997636daa9f32d2c670c267b745e73e963cd11
luishark2 <50499542+luishark2@users.noreply.github.com>
2026-02-19 10:24:53 -0600

    Create index.js

---
+++ test/index.js
@@ -0,0 +1,14 @@
+const sum = require('../');
+const assert = require('assert');
+
+describe('Sum', () => {
+  it('should handle positive numbers', () => {
+    asser.equals(sum(3, 5), 8);
+  });
+  it('should handle negative numbers', () => {
+    asser.equals(sum(3, -5), -2);
+  });
+  it('should handle decimals', () => {
+    asser.equals(sum(3.5, 7.1), 10.6);
+  });
+)};
